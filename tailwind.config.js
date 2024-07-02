87042ed7afa2012ac1f3295e8f1473ff253a8a7f
The Ace <udonsichisom02@gmail.com>
2024-07-02 17:52:13 +0100

    feat: initialized project

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neutral: {
+          100: "#FFFFFF",
+          200: "#F7F6FF",
+          300: "#F6F6F6",
+          400: "#595959",
+          500: "#262626",
+          900: "#191635",
+        },
+        primary: {
+          400: "#F0F4FE",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        euclid: ["'Euclid Circular B'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
